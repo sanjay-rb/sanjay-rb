62eddc78425722d306b843dff141d16b2417843f
sanjaysanju618 <sanjaybabu618@gmail.com>
2021-11-08 13:41:37 +0530

    manual check

--- app/razorpay_payment/verify.py
+++ app/razorpay_payment/verify.py
@@ -1,16 +1,22 @@
 from flask import request
-from app import app, payment_client
+from app import app
+import hmac
+import hashlib
 
 @app.route("/verify", methods=['POST'])
 def verify():
     if request.method.lower() == 'post':
         try:
             params_dict = request.get_json()
-            print('params_dict', payment_client.utility.verify_payment_signature(params_dict))
-            return {
-                'isVerified' : True
-            }
+            data = params_dict['razorpay_order_id'] + "|" + params_dict['razorpay_payment_id']
+            signature = hmac.new(
+                str(API_SECRET),
+                msg=data,
+                digestmod=hashlib.sha256
+            ).hexdigest().lower()
+            if params_dict['razorpay_signature'] == signature:
+                return { 'isVerified' : True }
+            else:
+                return { 'isVerified' : False }   
         except Exception as e:
-            return {
-                'isVerified' : False
-            }
+            return { 'isVerified' : False }
