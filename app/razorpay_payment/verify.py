99a84d510a706872110c4b6c91e558ddabb2f06b
sanjaysanju618 <sanjaybabu618@gmail.com>
2021-11-08 13:44:23 +0530

    print statements

--- app/razorpay_payment/verify.py
+++ app/razorpay_payment/verify.py
@@ -8,12 +8,15 @@ def verify():
     if request.method.lower() == 'post':
         try:
             params_dict = request.get_json()
+            print('params', params_dict)
             data = params_dict['razorpay_order_id'] + "|" + params_dict['razorpay_payment_id']
             signature = hmac.new(
                 str(API_SECRET),
                 msg=data,
                 digestmod=hashlib.sha256
             ).hexdigest().lower()
+            print(signature)
+            print(params_dict['razorpay_signature'] == signature)
             if params_dict['razorpay_signature'] == signature:
                 return { 'isVerified' : True }
             else:
