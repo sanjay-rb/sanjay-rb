3fc3dcee4e621163a5c61a226ab7c79aa792f82e
sanjaysanju618 <sanjaybabu618@gmail.com>
2021-11-08 14:14:28 +0530

    test

--- app/razorpay_payment/verify.py
+++ app/razorpay_payment/verify.py
@@ -6,20 +6,17 @@ import hashlib
 @app.route("/verify", methods=['POST'])
 def verify():
     if request.method.lower() == 'post':
-        try:
-            params_dict = request.get_json()
-            print('params', params_dict)
-            data = params_dict['razorpay_order_id'] + "|" + params_dict['razorpay_payment_id']
-            signature = hmac.new(
-                api_secret.encode('utf-8'),
-                msg=data,
-                digestmod=hashlib.sha256
-            ).hexdigest().lower()
-            print(signature)
-            print(params_dict['razorpay_signature'] == signature)
-            if params_dict['razorpay_signature'] == signature:
-                return { 'isVerified' : True }
-            else:
-                return { 'isVerified' : False }   
-        except Exception as e:
-            return { 'isVerified' : False, 'error' : str(e) }
+        params_dict = request.get_json()
+        print('params', params_dict)
+        data = params_dict['razorpay_order_id'] + "|" + params_dict['razorpay_payment_id']
+        signature = hmac.new(
+            api_secret.encode(),
+            msg=data.encode(),
+            digestmod=hashlib.sha256
+        ).hexdigest().lower()
+        print(signature)
+        print(params_dict['razorpay_signature'] == signature)
+        if params_dict['razorpay_signature'] == signature:
+            return { 'isVerified' : True }
+        else:
+            return { 'isVerified' : False } 
